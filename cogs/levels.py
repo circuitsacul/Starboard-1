aa08e01bde816b8e386a428e5ff91f7d1a8160ed
Lucas <circuitsacul@gmail.com>
2020-10-03 08:53:35 -0400

    Added leaderboard command

--- cogs/levels.py
+++ cogs/levels.py
@@ -1,4 +1,4 @@
-import discord, bot_config
+import discord, bot_config, disputils
 from discord.ext import commands
 from events import leveling
 from typing import Union
@@ -39,6 +39,50 @@ class Levels(commands.Cog):
         embed.set_footer(text=f"Total Received: {received}\nTotal Given: {given}")
 
         await ctx.send(embed=embed)
+
+    @commands.command(
+        name='leaderboard', aliases=['lb', 'levels'],
+        description='View users in order of their XP',
+        brief='View leaderboard'
+    )
+    @commands.guild_only()
+    async def show_leaderboard(self, ctx):
+        get_members = \
+            """SELECT * FROM members WHERE xp != 0 AND guild_id=$1"""
+
+        conn = await self.bot.db.connect()
+        async with self.bot.db.lock and conn.transaction():
+            members = await conn.fetch(get_members, ctx.guild.id)
+        await conn.close()
+        _ordered = sorted(members, key=lambda m: m['xp'], reverse=True)
+        ordered = []
+        for m in _ordered:
+            mobject = discord.utils.get(ctx.guild.members, id=m['user_id'])
+            if mobject is None or mobject.bot:
+                continue
+            else:
+                username = mobject.name
+            ordered.append({'name': username, 'd': m})
+
+        stringed = [
+            f"__**{m['name']}**__: **Level {m['d']['lvl']} | XP {m['d']['xp']}**\n"
+            for m in ordered
+        ]
+        size = 10
+        grouped = [stringed[i:i+size] for i in range(0, len(stringed), size)]
+
+        embeds = []
+        for group in grouped:
+            string = ""
+            embed = discord.Embed(title=f"Leaderboard for {ctx.guild.name}", color=bot_config.COLOR)
+            embed.set_thumbnail(url="https://i.ibb.co/CQvbvDq/trophy-1f3c6.png")
+            for m in group:
+                string += m
+            embed.description = string
+            embeds.append(embed)
+
+        paginator = disputils.BotEmbedPaginator(ctx, embeds)
+        await paginator.run()
 
     @commands.command(
         name='reset', brief='Resets XP and Levels for a user',
