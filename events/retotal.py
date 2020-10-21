7103ce3b3aad4f80456c8bb062fef12e0e4da893
Lucas <circuitsacul@gmail.com>
2020-10-21 19:44:45 -0400

    Fix slowdown problem

--- events/retotal.py
+++ events/retotal.py
@@ -1,6 +1,27 @@
 import functions
 
 
+async def is_starboard_emoji(db, guild_id, emoji):
+    emoji = str(emoji)
+    get_starboards = \
+        """SELECT * FROM starboards WHERE guild_id=$1"""
+    get_sbeemojis = \
+        """SELECT * FROM sbemojis WHERE starboard_id=$1"""
+
+    async with db.lock:
+        conn = await db.connect()
+        async with conn.transaction():
+            starboards = await conn.fetch(get_starboards, guild_id)
+            all_emojis = []
+            for starboard in starboards:
+                emojis = await conn.fetch(get_sbeemojis, starboard['id'])
+                all_emojis += [
+                    str(e['name']) if e['d_id'] is None
+                    else str(e['d_id']) for e in emojis
+                ]
+    return str(emoji) in all_emojis
+
+
 # Check and recount stars on a message when necessary
 async def needs_recount(bot, message):
     get_reactions = \
@@ -10,7 +31,7 @@ async def needs_recount(bot, message):
     for r in message.reactions:
         total += r.count
 
-    if total <= 1:  # Don't recount if the message only has 1 reaction (or 0)
+    if total <= 3:  # Don't recount if the message only has 1 reaction (or 0)
         return False
 
     async with bot.db.lock:
@@ -36,6 +57,7 @@ async def recount_reactions(bot, message):
     # hard to explain why, but I also remove the message
     # from the cache when recounting the stars on it
     await bot.db.cache.remove(message.id, message.guild.id)
+    message = await functions.fetch(bot, message.id, message.guild.id)
 
     # [{'user_id': user_id, 'name': name}, ...]
     # other values can be determined from the message object
@@ -46,6 +68,12 @@ async def recount_reactions(bot, message):
             name = str(reaction.emoji.id)
         else:
             name = str(reaction.emoji)
+
+        if not await is_starboard_emoji(bot.db, message.guild.id, name):
+            print("Not starboard emoji")
+            continue
+        print("Starboard emoji")
+
         async for user in reaction.users():
             if user is None:
                 continue
