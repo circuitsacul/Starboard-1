afd7d0728f04e671f525520860289366946eee1a
Lucas <circuitsacul@gmail.com>
2020-10-21 21:45:34 -0400

    Remove print statement

--- events/retotal.py
+++ events/retotal.py
@@ -52,7 +52,6 @@ async def needs_recount(bot, message):
 
 
 async def recount_reactions(bot, message):
-    print("Retotaling")
     check_reaction = \
         """SELECT * FROM reactions WHERE
         message_id=$1 AND name=$2 AND user_id=$3"""
