c3605cd7e95ed8ee41faa0ae9d474dc2d6a7c844
Lucas <circuitsacul@gmail.com>
2020-11-01 11:10:22 -0500

    slight modification

--- cogs/aschannels.py
+++ cogs/aschannels.py
@@ -11,11 +11,11 @@ class AutoStarChannels(commands.Cog):
 
     @commands.group(
         name='aschannels', aliases=['asc', 'as'],
-        description="List all AutoStarChannels, and add or remove them.",
-        brief="List AutoStarChannels", invoke_without_command=True
+        description="Manage AutoStar Channels",
+        brief="Manage AutoStar Channels", invoke_without_command=True
     )
     @commands.guild_only()
-    async def list_aschannels(self, ctx):
+    async def aschannels(self, ctx):
         get_aschannels = \
             """SELECT * FROM aschannels WHERE guild_id=$1"""
 
@@ -46,7 +46,7 @@ class AutoStarChannels(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    @list_aschannels.command(
+    @aschannels.command(
         name='add', aliases=['a'],
         description='Sets a channel as an AutoStarChannel',
         breif='Add an AutoStarChannel'
@@ -56,7 +56,7 @@ class AutoStarChannels(commands.Cog):
     async def add_aschannel(self, ctx, channel: discord.TextChannel):
         pass
 
-    @list_aschannels.command(
+    @aschannels.command(
         name='remove', aliases=['r', 'delete', 'del', 'd'],
         description="Remove an AutoStarChannel",
         brief="Remove an AutoStarChannel"
