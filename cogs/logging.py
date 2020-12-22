b2d84e114eb8695a460cd2007420345b134e38f3
Lucas <circuitsacul@gmail.com>
2020-12-22 16:42:08 -0500

    add caught exception

--- cogs/logging.py
+++ cogs/logging.py
@@ -113,7 +113,8 @@ class Logging(commands.Cog):
             errors.NotOwner, errors.CommandOnCooldown,
             errors.ChannelNotFound, errors.BadUnionArgument,
             errors.BotMissingPermissions, errors.UserNotFound,
-            errors.MemberNotFound, discord.InvalidArgument
+            errors.MemberNotFound, discord.InvalidArgument,
+            errors.RoleNotFound
         ]:
             pass
         elif type(error) is discord.ext.commands.errors.MaxConcurrencyReached:
